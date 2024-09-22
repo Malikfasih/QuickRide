fba6c740fb0c599d296889e85d918010fc937695
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2024-09-22 13:36:29 +0500

    Initialize project

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello Uber Client!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
