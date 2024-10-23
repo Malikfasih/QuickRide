204c54b53d60271803f5c0680ae92d737af206ca
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2024-10-23 12:50:35 +0500

    initialize book-ride, confirm-ride and find-ride pages

---
+++ app/(root)/confirm-ride.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const ConfirmRide = () => {
+  return (
+    <SafeAreaView>
+      <Text>ConfirmRide</Text>;
+    </SafeAreaView>
+  );
+};
+
+export default ConfirmRide;
---
+++ app/(root)/find-ride.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const FindRide = () => {
+  return (
+    <SafeAreaView>
+      <Text>FindRide</Text>;
+    </SafeAreaView>
+  );
+};
+
+export default FindRide;
